c31a2414159905596aae88dc30b460bbbf160589
Lloyd S Turley <78443727+lloydturley@users.noreply.github.com>
2021-10-27 10:20:58 -0400

    added CL input and loop for input

--- Program.cs
+++ Program.cs
@@ -10,118 +10,172 @@ namespace TestPrep
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Armstrong();
-            //PrimeNumbers(1000);
-            //LeapYear();
-            OddEven(100);
-            //Palindrome(args);
-            StrongNumber(args);
-            Console.ReadLine();
-            
-        }
-        static void StrongNumber(string[] input)
-        {
-            foreach (var item in input)
+            int intInput;
+
+            Console.WriteLine("Welcome to Test Prep");
+            while (true)
             {
-                int finalCalcd = 0;
-                char[] chars = item.ToCharArray();
-                foreach (var chr in chars)
+                Console.WriteLine("Please enter a value");
+                string input = Console.ReadLine();
+                var typedArgs = input.Split(' ');
+                foreach (var item in typedArgs)
                 {
-                    int calculated = 1;
-                    int digit = int.Parse(chr.ToString());
-                    int counter = 1;
-                    while(counter < digit + 1)
+                    if (int.TryParse(item, out intInput))
                     {
-                        calculated *= counter;
-                        counter++;
+                        AbundantNumber(intInput);
+                        Automorphic(intInput);
+                        PrimeNumbers(intInput);
+                        LeapYear(intInput);
+                        OddEven(intInput);
+                        StrongNumber(intInput);
                     }
-                    finalCalcd += calculated;
-                }
-
-                if (item == finalCalcd.ToString())
-                {
-                    Console.WriteLine("You have a strong number");
-                } else
-                {
-                    Console.WriteLine($"You do not have a strong #. Calcd:  {finalCalcd}");
+                    else if (item.Length != 0)
+                    {
+                        Palindrome(item);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input");
+                    }
                 }
             }
         }
-        static void Palindrome(string[] input)
+
+        private static void AbundantNumber(int input)
         {
-            var words = input.ToList();
-            foreach (var word in words)
+            Console.WriteLine("Abundant Number Evaluation");
+            int sum = 0;
+            int iterationCount = 0;
+            while (++iterationCount < input)
             {
-                if (word == new string(word.ToCharArray().Reverse().ToArray()))
+                if (input % iterationCount == 0)
                 {
-                    Console.WriteLine($"{word} is a palindrome");
+                    sum += iterationCount;
+                    if (sum > input) { break; }
                 }
-                int len = word.Length;
-                
-                string reversed = "";
-
-                for (int i = len; i > 0; i--)
-                {
-                    reversed = reversed + word.Substring(i-1, 1);
-                }
-                if (word == reversed)
-                {
-                    Console.WriteLine($"{word} is a palindrome #2");
-                } else
-                {
-                    Console.WriteLine($"{word} is not a palindrome");
-                }
-
+            }
+            if (sum > input)
+            {
+                Console.WriteLine($"----{input} is an Abundant Number");
+            } else
+            {
+                Console.WriteLine($"----{input} is Not Abundant Number");
             }
         }
+        static void StrongNumber(int input)
+        {
+            Console.WriteLine("Strong Number Evaluation");
+            int finalCalcd = 0;
+            char[] chars = input.ToString().ToCharArray();
+            foreach (var chr in chars)
+            {
+                int calculated = 1;
+                int digit = int.Parse(chr.ToString());
+                int counter = 1;
+                while(counter < digit + 1)
+                {
+                    calculated *= counter;
+                    counter++;
+                }
+                finalCalcd += calculated;
+            }
+
+            if (input == finalCalcd)
+            {
+                Console.WriteLine($"----{input} is a strong number");
+            } else
+            {
+                Console.WriteLine($"----{input} is not strong number");
+            }
+        }
+        static void Palindrome(string input)
+        {
+            Console.WriteLine("Palindrome Evaluation");
+            if (input == new string(input.ToCharArray().Reverse().ToArray()))
+            {
+                Console.WriteLine($"{input} is a palindrome");
+            }
+            int len = input.Length;
+                
+            string reversed = "";
+
+            for (int i = len; i > 0; i--)
+            {
+                reversed = reversed + input.Substring(i-1, 1);
+            }
+            if (input == reversed)
+            {
+                Console.WriteLine($"----{input} is a palindrome #2");
+            } else
+            {
+                Console.WriteLine($"----{input} is not a palindrome");
+            }
+
+        }
+        
         static void OddEven(int max)
         {
+            Console.WriteLine($"Display Even Numbers from 0 to {max}");
             List<string> list = new List<string>();
             
             for (int i = 0; i < max; i++)
             {
                 if (i % 2 == 0)
                 {
-                    //Console.WriteLine($"{i} is even");
                     list.Add(i.ToString());
-                    
-                } else
-                {
-                    //Console.WriteLine($"{i} is odd");
                 }
             }
+            int count=-1;
             foreach (var item in list)
             {
-                Console.Write($"{item},");
+                if (++count!=list.Count)
+                {
+                    Console.Write($"----{item},");
+                } else
+                {
+                    Console.Write($"----{item}");
+                }
             }
         }
-        static void LeapYear()
+        static void LeapYear(int input)
         {
+            DateTime input2date = DateTime.Parse($"01/01/{input}");
+            
+            Console.WriteLine($"Display Leap Years from 0000 AD to {input2date.Year} AD");
             int start = 0;
-            int end = 2030;
 
-            for (int i = start; i < end; i++)
+            for (int i = start; i < input2date.Year; i++)
             {
                 DateTime currentOne = DateTime.Parse($"01/01/{i}");
                 var curY = currentOne.Year;
 
                 if (curY % 4 == 0)
                 {
-                    Console.WriteLine($"{currentOne.Year}");
+                    Console.WriteLine($"----{currentOne.Year}");
                 }
             }
         }
 
-        static void Armstrong()
+        static void Automorphic(int input)
         {
-            Console.WriteLine("Inside Armstrong");
+            Console.WriteLine("Automorphic Evaluation");
+            var calcdVal = input * input;
+
+            string rightCharacter = calcdVal.ToString().Substring(calcdVal.ToString().Length-1,1);
+
+            if (rightCharacter == input.ToString())
+            {
+                Console.WriteLine($"----{input} is an Automorphic Number");
+            } else
+            {
+                Console.WriteLine($"----{input} is Not an Automorphic Number");
+            }
         }
 
         static void PrimeNumbers(int max)
         {
             int x = 2;
-            Console.WriteLine($"Inside Prime Number. Will print all the prime numbers between {x} and {max}");
+            Console.WriteLine($"Prime Number Evaluation. Will print all the prime numbers between {x} and {max}");
 
             while(x < max + 1)
             {
@@ -134,7 +188,7 @@ namespace TestPrep
                     }
                 }
                 if (count < 2) {
-                    Console.WriteLine($"{x} is a prime");
+                    Console.WriteLine($"----{x} is a prime");
                 }
                 x++;
             }
