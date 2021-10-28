fafd605d1f02964ce089e90ad36364a65a0cffca
Lloyd S Turley <78443727+lloydturley@users.noreply.github.com>
2021-10-28 13:33:40 -0400

    added remove special characters feature

--- Program.cs
+++ Program.cs
@@ -10,62 +10,76 @@ namespace TestPrep
     class Program
     {
         public readonly static Writer _writer = new Writer();
-        public readonly static string file = "C:/temp/TestPrepResults.txt";
+        public readonly static string file = "c:/temp/TestPrepResults.txt";
         static void Main(string[] args)
         {
-            int intInput;
-            AbundantNumber aNum = new AbundantNumber();
-            AutoMorphic autoM = new AutoMorphic();
-            PrimeNumbers primeNum = new PrimeNumbers();
-            LeapYear leapY = new LeapYear();
-            OddEven oE = new OddEven();
-            StrongNumber strongNum = new StrongNumber();
-            Palindrome pal = new Palindrome();
-            ReverseString reverse = new ReverseString();
-
-            Console.WriteLine("Welcome to Test Prep");
-            Console.WriteLine($"Results will be written to {file.Replace("/", "\\")}");
-            if (File.Exists(file)) { File.Delete(file); }
-            while (true)
+            try
             {
-                Console.WriteLine("Please enter a value");
+                int intInput;
+                AbundantNumber aNum = new AbundantNumber();
+                AutoMorphic autoM = new AutoMorphic();
+                PrimeNumbers primeNum = new PrimeNumbers();
+                LeapYear leapY = new LeapYear();
+                OddEven oE = new OddEven();
+                StrongNumber strongNum = new StrongNumber();
+                Palindrome pal = new Palindrome();
+                ReverseString reverse = new ReverseString();
+                RemoveSpecialCharacters removeSp = new RemoveSpecialCharacters();
 
-                string input = Console.ReadLine();
-                var typedArgs = input.Trim().Split(' ');
-
-                foreach (var item in typedArgs)
+                Console.WriteLine("Welcome to Test Prep");
+                Console.WriteLine($"Results will be written to {file.Replace("/", "\\")}");
+                if (File.Exists(file)) { File.Delete(file); }
+                while (true)
                 {
-                    if (int.TryParse(item, out intInput))
+                    Console.WriteLine("Please enter a value");
+
+                    string input = Console.ReadLine();
+                    input = removeSp.Remove(input);
+                    var typedArgs = input.Trim().Split(' ');
+
+                    foreach (var item in typedArgs)
                     {
-                        _writer.Write(file, "Abundant Number Evaluation");
-                        _writer.Write(file, aNum.Calculate(intInput));
-                        _writer.Write(file, "Automorphic Evaluation");
-                        _writer.Write(file, autoM.Calculate(intInput));
-                        _writer.Write(file, $"Prime Number Evaluation. Will print all the prime numbers between 2 and {intInput}");
-                        _writer.Write(file, primeNum.Calculate(intInput));
-                        _writer.Write(file, $"Display Leap Years from 0001 AD to {intInput} AD");
-                        _writer.Write(file, leapY.Calculate(intInput));
-                        _writer.Write(file, $"Display Even Numbers from 0 to {intInput}");
-                        _writer.Write(file, oE.Calculate(intInput));
-                        _writer.Write(file, "Strong Number Evaluation");
-                        _writer.Write(file, strongNum.Calculate(intInput));
-                        Console.WriteLine("Process is complete!");
-                    }
-                    else if (item.Length != 0)
-                    {
-                        //item.All(Char.IsLetter())
-                        _writer.Write(file, "Palindrome Evaluation");
-                        _writer.Write(file, pal.Calculate(item));
-                        _writer.Write(file, "Reverse String");
-                        _writer.Write(file, reverse.Reverse(item).ToString());
-                        Console.WriteLine("Process is complete!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input");
+                        _writer.Write(file, item);
+                        if (int.TryParse(item, out intInput))
+                        {
+                            _writer.Write(file, "Abundant Number Evaluation");
+                            _writer.Write(file, aNum.Calculate(intInput));
+                            _writer.Write(file, "Automorphic Evaluation");
+                            _writer.Write(file, autoM.Calculate(intInput));
+                            _writer.Write(file, $"Prime Number Evaluation. Will print all the prime numbers between 2 and {intInput}");
+                            _writer.Write(file, primeNum.Calculate(intInput));
+                            _writer.Write(file, $"Display Leap Years from 0001 AD to {intInput} AD");
+                            _writer.Write(file, leapY.Calculate(intInput));
+                            _writer.Write(file, $"Display Even Numbers from 0 to {intInput}");
+                            _writer.Write(file, oE.Calculate(intInput));
+                            _writer.Write(file, "Strong Number Evaluation");
+                            _writer.Write(file, strongNum.Calculate(intInput));
+                            Console.WriteLine("Process is complete!");
+                        }
+                        else if (item.Length != 0)
+                        {
+                            //item.All(Char.IsLetter())
+                            _writer.Write(file, "Palindrome Evaluation");
+                            _writer.Write(file, pal.Calculate(item));
+                            _writer.Write(file, "Reverse String");
+                            _writer.Write(file, reverse.Reverse(item).ToString());
+                            _writer.Write(file, "Remove Special Chars");
+                            _writer.Write(file, removeSp.Remove(item));
+                            Console.WriteLine("Process is complete!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input");
+                        }
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
         }
     }
 }
---
+++ RemoveSpecialCharacters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPrep
+{
+    public class RemoveSpecialCharacters
+    {
+        public string Remove(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char item in input)
+            {
+                if(
+                    (item >= '0' && item <= '9') || 
+                    (item >= 'A' && item <= 'Z') || 
+                    (item >= 'a' && item <= 'z') ||
+                    (item == ' ')
+                    )
+                {
+                    sb.Append(item);
+                }
+            }
+
+            return sb.ToString();
+
+        }
+    }
+}
